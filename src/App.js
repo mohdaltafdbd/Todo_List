9b0b16de66e1e4ff90f6e99ccef9010f66445db7
mohdaltafdbd <112413513+mohdaltafdbd@users.noreply.github.com>
2023-02-19 03:45:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addTodo(e.target.todo.value);
+          e.target.todo.value = "";
+        }}
+      >
+        <input type="text" name="todo" />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
